48c6e708897527bebea044f0020282e09add03b7
catonrug <aigars.kadikis@gmail.com>
2016-02-23 20:28:08 +0200

    next

--- check.sh
+++ check.sh
@@ -263,8 +263,8 @@ echo changes: "$changes"
 
 echo looking for change log..
 wget -qO- "$changes" | grep "$version"
-wget -qO- "$changes" | grep -A99 "$versioncheck" | grep -B99 -m2 "ActivePresenter" | grep -v "<\/h2>" | sed -e "s/<[^>]*>//g" | sed ";s/^[ \t]*//g" | grep "[a-zA-Z]" | sed -e "/:/! s/^/- /"
-wget -qO- "$changes" | grep -A99 "$versioncheck" | grep -B99 -m2 "ActivePresenter" | grep -v "<\/h2>" | sed -e "s/<[^>]*>//g" | sed ";s/^[ \t]*//g" | grep "[a-zA-Z]" | sed -e "/:/! s/^/- /" > $tmp/change.log
+wget -qO- "$changes" | grep -A99 "$version" | grep -B99 -m2 "ActivePresenter" | grep -v "<\/h2>" | sed -e "s/<[^>]*>//g" | sed ";s/^[ \t]*//g" | grep "[a-zA-Z]" | sed -e "/:/! s/^/- /"
+wget -qO- "$changes" | grep -A99 "$version" | grep -B99 -m2 "ActivePresenter" | grep -v "<\/h2>" | sed -e "s/<[^>]*>//g" | sed ";s/^[ \t]*//g" | grep "[a-zA-Z]" | sed -e "/:/! s/^/- /" > $tmp/change.log
 
 echo change log is:
 cat $tmp/change.log
