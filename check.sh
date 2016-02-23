727442d40a089f922299ddcdebf2d254528118f4
catonrug <aigars.kadikis@gmail.com>
2016-02-23 20:02:56 +0200

    next

--- check.sh
+++ check.sh
@@ -262,6 +262,7 @@ echo version: $versioncheck
 echo changes: "$changes"
 
 echo looking for change log..
+wget -qO- "$changes"
 wget -qO- "$changes" | grep -A99 -m1 "$versioncheck" | grep -B99 -m2 "ActivePresenter" | grep -v "<\/h2>" | sed -e "s/<[^>]*>//g" | sed ";s/^[ \t]*//g" | grep "[a-zA-Z]" | sed -e "/:/! s/^/- /"
 wget -qO- "$changes" | grep -A99 -m1 "$versioncheck" | grep -B99 -m2 "ActivePresenter" | grep -v "<\/h2>" | sed -e "s/<[^>]*>//g" | sed ";s/^[ \t]*//g" | grep "[a-zA-Z]" | sed -e "/:/! s/^/- /" > $tmp/change.log
 
