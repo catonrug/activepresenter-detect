6e460363a43ff23096c05694984c3ca595cd1e22
catonrug <aigars.kadikis@gmail.com>
2016-03-07 20:26:58 +0200

    next

--- check.sh
+++ check.sh
@@ -54,6 +54,12 @@ if [ ! -d "../gd" ]; then
   mkdir -p "../gd"
 fi
 
+if [ -f ~/uploader_credentials.txt ]; then
+sed "s/folder = test/folder = `echo $appname`/" ../uploader.cfg > ../gd/$appname.cfg
+else
+echo google upload will not be used cause ~/uploader_credentials.txt do not exist
+fi
+
 #this link provides latest version
 link=$(echo http://atomisystems.com/apdownloads/latest/ActivePresenter_setup.exe)
 
